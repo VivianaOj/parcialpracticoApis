c3d84e7e4fc0f3e95c5b62c6ec63bffd4b905790
Leidy Viviana Osorio <viviana@movingsuppliesusa.com>
2024-09-15 22:57:25 -0500

    Add validacion de campos

---
+++ src/shared/utils/validation.utils.ts
@@ -0,0 +1,16 @@
+const articles = [
+  ['clubs', 'categoria', 'ciudad', 'receta'],
+  ['socio', 'producto', 'restaurante'],
+];
+
+export const getMissingFields = (errorMsgs) => {
+  return errorMsgs
+    .filter((msg: string) => msg.includes('should not be empty'))
+    .map((msg: string) => msg.split(' ')[0]);
+};
+
+export const requiredFieldsMsg = (missingFields) =>
+  `Los siguientes campos son obligatorios: ${missingFields.join(', ')}.`;
+
+export const unknownMsg = (resource) =>
+  `${articles[0].includes(resource) ? 'La' : 'El'} ${resource} con el id dado no existe.`;
